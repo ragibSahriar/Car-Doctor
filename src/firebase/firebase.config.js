064d90c357f4297306f91775b48420c995dbf7eb
seabeach11 <seebsk9@gmail.com>
2023-05-15 02:20:37 +0600

    added home

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,8 +2,8 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey:import.meta.env.VITE_apiKey,
   authDomain:import.meta.env.VITE_authDomain,
@@ -12,6 +12,24 @@ const firebaseConfig = {
   messagingSenderId:import.meta.env.VITE_messagingSenderId,
   appId:import.meta.env.VITE_appId
 };
+
+
+// const firebaseConfig = {
+
+//   apiKey: "AIzaSyC3okm_lP9_pqH6dS7z-8RGcxq8-qep4vQ",
+
+//   authDomain: "cars-doctor-5fa27.firebaseapp.com",
+
+//   projectId: "cars-doctor-5fa27",
+
+//   storageBucket: "cars-doctor-5fa27.appspot.com",
+
+//   messagingSenderId: "1094443277182",
+
+//   appId: "1:1094443277182:web:af429112b1c1aff9ba712f"
+
+// };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { RouterProvider } from "react-router-dom";
+import { Link, RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes.jsx";
 import AuthProvider from "./Providers/AuthProvider";
 
